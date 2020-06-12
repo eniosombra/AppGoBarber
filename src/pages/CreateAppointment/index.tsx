a7160db47c9e1bede955fee7bc1b04254672b9b3
Enio L. Sombra <eniosombra@gmail.com>
2020-06-12 10:22:14 -0300

    Criação do Header do CreateAppointment

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+  const { user } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+
+  const { providerId } = route.params as RouteParams;
+
+  const navigateToBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
-    <View>
+    <Container>
+      <Header>
+        <BackButton onPress={navigateToBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
       <Text>CreateAppointment</Text>
-    </View>
+      <Text>{providerId}</Text>
+    </Container>
   );
 };
 
